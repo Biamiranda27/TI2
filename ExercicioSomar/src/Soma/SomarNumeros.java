4e005ef2dbc88c2df348aa4c07cdede9f7f8f9d8
Beatriz Miranda <biampv27@gmail.com>
2025-04-10 22:00:12 -0300

    Exercicio 1

---
+++ ExercicioSomar/src/Soma/SomarNumeros.java
@@ -0,0 +1,15 @@
+package Soma;
+import java.util.*;
+public class SomarNumeros {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1, n2;
+        System.out.println("digite o primeiro numero: ");
+        n1 = scanner.nextInt();
+        System.out.println("digite o segundo numero: ");
+        n2 = scanner.nextInt();
+        int soma = n1+n2;
+        System.out.println("A soma é: " +soma);
+        scanner.close();
+    }
+}
